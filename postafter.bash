25652fb5b287beca32bfd73bc68eb34dd844151f
tilnow <44685516+tilnow@users.noreply.github.com>
2021-02-24 13:36:14 +0200

    Update postafter.bash

--- postafter.bash
+++ postafter.bash
@@ -1,9 +1,8 @@
-mess_file=$(mktemp)
-echo "trying to post after prog stopped"
-echo '{"content": lets try to upload a results file "' >$mess_file
-echo $1 >>$mess_file
-#echo 'pid is' $APP_PID >>$mess_file
-echo '"}' >>$mess_file
-sed -i 's/$/\\n/' $mess_file
-sed -i '$s/..$//' $mess_file
-curl -i -H 'Expect: application/json' -F file=@$1 -F 'payload_json=@$mess_file' $TWEAKS_HOOK
+post_file=$(mktemp)
+echo "trying to post after prog stopped" $1
+echo '{"content": lets try to upload a results file "' >$post_file
+echo $1 >>$post_file
+echo '"}' >>$post_file
+sed -i 's/$/\\n/' $post_file
+sed -i '$s/..$//' $post_file
+curl -i -H 'Expect: application/json' -F file=@$1 -F 'payload_json=@$post_file' $TWEAKS_HOOK
