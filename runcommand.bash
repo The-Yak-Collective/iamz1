c6441c196beabd3be956f34260fdb494a21cec3a
tilnow <44685516+tilnow@users.noreply.github.com>
2021-02-24 12:48:20 +0200

    Update runcommand.bash

--- runcommand.bash
+++ runcommand.bash
@@ -3,10 +3,10 @@ echo "starting command" $0
 pwd
 temp_file=$(mktemp) #i am actually NOT sending the output yet
 mess_file=$(mktemp)
-source "$@" >temp_file &2>1 &
+source "$@" >temp_file 2>&1 &
 export APP_PID=$! 
 echo '{"content": "' >mess_file
-echo 'pid is' $APP_PID >mess_file
+echo 'pid is' $APP_PID >>mess_file
 echo '"}' >>mess_file
 sed -i 's/$/\\n/' mess_file
 sed -i '$s/..$//' mess_file
