733861788ad96fd4a9e63a14f326dd4ba3d066c0
tilnow <44685516+tilnow@users.noreply.github.com>
2021-02-24 13:47:27 +0200

    quote stuff

--- postafter.bash
+++ postafter.bash
@@ -1,9 +1,10 @@
 post_file=$(mktemp)
 echo "trying to post after prog stopped" $1
-echo '{"content": lets try to upload a results file "' >$post_file
+echo '{"content": "lets try to upload a results file ' >$post_file
 echo $1 >>$post_file
 echo '"}' >>$post_file
 sed -i 's/$/\\n/' $post_file
 sed -i '$s/..$//' $post_file
 astring=$(<$post_file)
-curl -i -H 'Expect: application/json' -F file=@$1 -F 'payload_json=$astring' $TWEAKS_HOOK
+echo $astring
+curl -i -H 'Expect: application/json' -F file=@$1 -F "payload_json=${astring}" $TWEAKS_HOOK
