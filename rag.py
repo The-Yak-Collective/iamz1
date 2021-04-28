c2dee7868b965b32f2e4fa4bb7240ba71c78671b
tilnow <44685516+tilnow@users.noreply.github.com>
2021-04-28 22:17:46 +0300

    rag cmd

---
+++ rag.py
@@ -0,0 +1,29 @@
+import sys
+import os
+import time
+import threading
+import ActionGroupControl as AGC
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+ACTDIR=os.getenv('ACTDIR','/home/pi/SpiderPi/ActionGroups/')
+
+print('''
+run action groups by name
+rag NAME TIMES
+''')
+args=sys.argv
+
+if(!len(argv)):
+    sys.exit("no action group specified")
+
+f=os.listdir(ACTDIR)
+actnames=[x for x in f if x[-4:]=='.d6a']
+
+times=1
+if len(args)>1:
+    times=int(args[1])
+name=args[0]
+if name in actnames:
+    AGC.runActionGroup(name,times=times)
+else:
+    sys.exit("action not exist: "+ name+"see"+"\n".join(actnames))
