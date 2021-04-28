6bb27105f61023b68ecf3830a02ee5635e16cc1c
tilnow <44685516+tilnow@users.noreply.github.com>
2021-04-28 15:43:53 +0300

    Update postafter.bash

--- postafter.bash
+++ postafter.bash
@@ -1,6 +1,6 @@
 post_file=$(mktemp)
 echo "trying to post after prog stopped" $1 $YAK_ROVER_NAME >trackingfile
-echo '{"wait": true, "content": "lets try to upload a results file ' $1 '"}'>$post_file
+echo '{"wait": true, "content": "' $YAK_ROVER_NAME ': lets try to upload a results file ' $1 '"}'>$post_file
 #echo $1 >>$post_file
 #echo '"}' >>$post_file
 sed -i 's/$/\\n/' $post_file
