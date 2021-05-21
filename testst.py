6a43330ae80a52b1e13506e7b0cedf2e127894ac
tilnow <44685516+tilnow@users.noreply.github.com>
2021-05-21 15:19:29 +0300

    add vloop, test stereo

---
+++ testst.py
@@ -0,0 +1,38 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+cam = cv2.VideoCapture(0)
+
+cv2.namedWindow("test-st")
+
+img_counter = 0
+
+while True:
+    ret, frame = cam.read()
+    if not ret:
+        print("failed to grab frame")
+        break
+    cv2.imshow("test", frame)
+
+    k = cv2.waitKey(1)
+    if k%256 == 27:
+        # ESC pressed
+        print("Escape hit, closing...")
+        break
+    elif k%256 == 32:
+        # SPACE pressed
+        img_name = "opencv_frame_{}.png".format(img_counter)
+        cv2.imwrite(img_name, frame)
+        print("{} written!".format(img_name))
+        img_counter += 1
+
+cam.release()
+cv2.destroyAllWindows()
+imgL = cv2.imread("opencv_frame_0.png",0)
+imgR = cv2.imread("opencv_frame_1.png",0)
+stereo = cv2.StereoBM_create(numDisparities=16, blockSize=15)
+disparity = stereo.compute(imgL,imgR)
+plt.imshow(disparity,'gray')
+plt.show()
+
