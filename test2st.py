4fb066ce0bb2b0c2c384d5f0972641ee0d01287a
tilnow <44685516+tilnow@users.noreply.github.com>
2021-05-21 16:02:49 +0300

    Update test2st.py

--- test2st.py
+++ test2st.py
@@ -1,19 +1,19 @@
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+import time
 
-cam = cv2.VideoCapture(0)
-
-cv2.namedWindow("test-st")
+start = time.time()
 
 img_counter = 0
 
 imgL = cv2.imread("opencv_frame_0.png",0)
 imgR = cv2.imread("opencv_frame_1.png",0)
 print("to here")
-stereo = cv2.StereoBM_create(numDisparities=32, blockSize=7)
+stereo = cv2.StereoBM_create(numDisparities=64, blockSize=11)
 print("now here")
 disparity = stereo.compute(imgL,imgR)
 print("and finally")
 cv2.imwrite("disp_img.png", disparity)
-cv2.destroyAllWindows()
+end = time.time()
+print(end - start)
