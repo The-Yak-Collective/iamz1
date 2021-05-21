23dfd76d19966500343b7afd49fe1e9e43d750dd
tilnow <44685516+tilnow@users.noreply.github.com>
2021-05-21 16:04:29 +0300

    Update test2st.py

--- test2st.py
+++ test2st.py
@@ -3,17 +3,21 @@ import numpy as np
 from matplotlib import pyplot as plt
 import time
 
-start = time.time()
+
 
 img_counter = 0
 
 imgL = cv2.imread("opencv_frame_0.png",0)
 imgR = cv2.imread("opencv_frame_1.png",0)
-print("to here")
-stereo = cv2.StereoBM_create(numDisparities=64, blockSize=11)
-print("now here")
+
+
+stereo = cv2.StereoBM_create(numDisparities=64, blockSize=5)
+
+start = time.time()
+
 disparity = stereo.compute(imgL,imgR)
-print("and finally")
-cv2.imwrite("disp_img.png", disparity)
+
+
 end = time.time()
 print(end - start)
+cv2.imwrite("disp_img.png", disparity)
