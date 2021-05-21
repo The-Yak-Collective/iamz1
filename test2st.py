b5d31ea03488cf377a6c2fb44e6ca137eae1fa59
tilnow <44685516+tilnow@users.noreply.github.com>
2021-05-21 16:12:06 +0300

    Update test2st.py

--- test2st.py
+++ test2st.py
@@ -11,7 +11,7 @@ imgL = cv2.imread("opencv_frame_0.png",0)
 imgR = cv2.imread("opencv_frame_1.png",0)
 
 
-stereo = cv2.StereoBM_create(numDisparities=64, blockSize=5)
+stereo = cv2.StereoBM_create(numDisparities=64, blockSize=5,fullDP=True)
 
 start = time.time()
 
