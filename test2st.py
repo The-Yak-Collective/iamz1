ce152ca9c3408ad35f3414caa58b9cdc2f807769
tilnow <44685516+tilnow@users.noreply.github.com>
2021-05-26 23:04:24 +0300

    generalize test2st

--- test2st.py
+++ test2st.py
@@ -2,13 +2,14 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 import time
+import sys
 
 
 
 img_counter = 0
 
-imgL = cv2.imread("opencv_frame_0.png",0)
-imgR = cv2.imread("opencv_frame_1.png",0)
+imgL = cv2.imread(sys.argv[1],0)
+imgR = cv2.imread(sys.argv[2],0)
 
 
 stereo = cv2.StereoSGBM_create(numDisparities=64, blockSize=5)
