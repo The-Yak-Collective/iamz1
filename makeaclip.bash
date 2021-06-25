68a8f8a34b700670d7629e6346b661820e6c6a4b
tilnow <44685516+tilnow@users.noreply.github.com>
2021-06-25 16:33:31 +0300

    Update makeaclip.bash

--- makeaclip.bash
+++ makeaclip.bash
@@ -1,6 +1,8 @@
 #make time a parameter
 #make location correct
-#make it detect dev2, dev0
+#make it detect dev2, dev0 (using error, maybe)
 #not debugged yet
-ffmpeg -y -t 3 -r 25 -i /dev/video0 a_clip.mp4
+ffmpeg -hide_banner -loglevel error -vf -y -t 3 -r 25 -i /dev/video0 a_clip.mp4
+echo $?
+ffmpeg -hide_banner -loglevel error -vf -y -t 3 -r 25 -i /dev/video2 a_clip.mp4
 echo $?
