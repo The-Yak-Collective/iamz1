2bc084cb11d6c5b8cdf5287eb5e0efc03ecf7e23
tilnow <44685516+tilnow@users.noreply.github.com>
2021-06-25 19:17:24 +0300

    yes

--- makeaclip.bash
+++ makeaclip.bash
@@ -3,11 +3,11 @@
 #make location correct
 #detect dev2, dev0 (using error, maybe)
 #mpeg -> mp4 is probably not best way
-ffmpeg -hide_banner -loglevel error -t 3 -i /dev/video0 a_clip.mpeg
+ffmpeg -hide_banner -loglevel error -y -t 3 -i /dev/video0 a_clip.mpeg
 echo $?
 if [ $? -gt 0 ]
 then
-    ffmpeg -hide_banner -loglevel error -t 3 -i /dev/video2 a_clip.mpeg
+    ffmpeg -hide_banner -loglevel error -y -t 3 -i /dev/video2 a_clip.mpeg
     echo $?
 fi
-ffmpeg -i a_clip.mpeg  -c:v libx264 a_clip.mp4
+ffmpeg -i -y a_clip.mpeg  -c:v libx264 a_clip.mp4
