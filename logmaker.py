69541c23bf667fcc2884f27600cf920a1e4472e0
tilnow <44685516+tilnow@users.noreply.github.com>
2021-07-16 14:04:23 +0300

    Update logmaker.py

--- logmaker.py
+++ logmaker.py
@@ -57,7 +57,7 @@ def main():
         while True:
             data=[]
             timestamp=read_funcs[0]() #returns timestamp
-            data[0]=timestamp
+            data=list(timestamp)
             for x in read_funcs[1:]:
                 data=data+x()
             csvwrite(f,data)
