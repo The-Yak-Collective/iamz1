3f30509998dc921b52833f6af56bc7c10982343b
tilnow <44685516+tilnow@users.noreply.github.com>
2021-08-11 16:52:25 +0300

    Update slowwave.py

--- slowwave.py
+++ slowwave.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 #slowwave
 import Board
+import time
+t=3000
 while True:
-  Board.setBusServoPulse(1,200,3000)
-  Board.setBusServoPulse(1,900,3000)
+  Board.setBusServoPulse(1,200,t)
+  time.sleep(t/1000.0)
+  Board.setBusServoPulse(1,900,t)
+  time.sleep(t/1000.0)
