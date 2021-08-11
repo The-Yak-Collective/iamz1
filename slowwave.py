bd7a68143de04b0216a787139366ff97445c7a68
tilnow <44685516+tilnow@users.noreply.github.com>
2021-08-11 17:06:26 +0300

    Update slowwave.py

--- slowwave.py
+++ slowwave.py
@@ -6,8 +6,9 @@ t=3000
 while True:
   Board.setBusServoPulse(1,200,t)
   time.sleep(t/2000.0)
-  x=Board.getBusServoPulse(1)
-  print(x)
+  for i in range(1,19):
+    x=Board.getBusServoPulse(i)
+    print(x)
   time.sleep(t/2000.0)
   Board.setBusServoPulse(1,900,t)
   time.sleep(t/1000.0)
