c019ab0dede77cb9c6a08f22d432816fd478618f
tilnow <44685516+tilnow@users.noreply.github.com>
2021-09-01 19:53:21 +0300

    split slowwave for testing

---
+++ slowread.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+#slowwave - moved read to "slowread.py"
+import Board
+import time
+import datetime
+t=3000
+while True:
+    print("start:",datetime.now())
+    for i in range(1,19):
+        x=Board.getBusServoPulse(i)
+        print(x)
+    print("end:",datetime.now())
+    time.sleep(0.1)
+
--- slowwave.py
+++ slowwave.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-#slowwave
+#slowwave - moved read to "slowread.py"
 import Board
 import time
 t=3000
 while True:
   Board.setBusServoPulse(1,200,t)
   time.sleep(t/2000.0)
-  for i in range(1,19):
-    x=Board.getBusServoPulse(i)
-    print(x)
+  #for i in range(1,19):
+  #  x=Board.getBusServoPulse(i)
+  #  print(x)
   time.sleep(t/2000.0)
   Board.setBusServoPulse(1,900,t)
   time.sleep(t/1000.0)
