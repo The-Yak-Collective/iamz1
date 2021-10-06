b9d3f858c6dfa0315dcd36e09bc200237c384355
tilnow <44685516+tilnow@users.noreply.github.com>
2021-10-06 15:12:11 +0300

    Update test_2303.py

--- test_2303.py
+++ test_2303.py
@@ -76,6 +76,7 @@ while True:
                 readitem.append(ord(chksum))
                 print(readitem.hex(), chksum, checksum(readitem), end=" ")
                 line=[x for x in coms if x[1]==ord(cmd)]
+                print("line=",line)
                 if len(line)==0:
                     print("illegal command")
                     continue
