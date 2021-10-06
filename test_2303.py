646cbf1c9ef227b71131f0d73bbb022d4590c90d
tilnow <44685516+tilnow@users.noreply.github.com>
2021-10-06 10:30:34 +0300

    Update test_2303.py

--- test_2303.py
+++ test_2303.py
@@ -1,10 +1,14 @@
 import serial
 import io
+import time
 
 ser=serial.Serial('/dev/ttyUSB0', 115200,xonxoff=False, rtscts=False, dsrdtr=False)
 ser.flushInput()
 ser.flushOutput()
 while True:
     bytesToRead = ser.inWaiting()
-    data_raw = ser.read(bytesToRead)
-    print(data_raw.hex(), end=" ")
+    if bytesToRead >0:
+        data_raw = ser.read(bytesToRead)
+        print(data_raw.hex(), end=" ")
+    else:
+        time.sleep(0.01)
