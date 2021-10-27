ef64c9895144863d5f806849571cd22a0576d312
tilnow <44685516+tilnow@users.noreply.github.com>
2021-10-27 14:33:48 +0300

    Update logrpc.py

--- logrpc.py
+++ logrpc.py
@@ -67,7 +67,7 @@ def tick():
             data=data+read6dof()
             data=data+readulsrangefinder()
             writer.writerow(data)
-            eventdata=eventdata+data
+            eventdata.append(data)
             t2=time.time()
             timelefttosleep=st+0.1-t2
             if timelefttosleep>0:
