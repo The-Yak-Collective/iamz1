281bcacda24ac9d3d9100aa5d29fb29619c386d4
tilnow <44685516+tilnow@users.noreply.github.com>
2021-11-03 14:47:34 +0200

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -1,10 +1,16 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
+listofrag=os.listdir("~/SpiderPi/ActionGroups")
+listofrag=[f[:-4] for f in listofrag]
+listofrag=list(set(listofrag))
 
 @app.route('/')
 def index():
-    return 'Hello world'
+    global listofrag
+    return " ".join(listofrag)
+@app.route('/rag?name=
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
