417837406ecea827879dba58f17067a6158c03e5
tilnow <44685516+tilnow@users.noreply.github.com>
2021-11-03 15:23:12 +0200

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 listofrag=os.listdir("/home/pi/SpiderPi/ActionGroups")
@@ -9,10 +9,16 @@ listofrag=list(set(listofrag))
 @app.route('/')
 def index():
     global listofrag
-    return " ".join(listofrag)
+    buts=[ragbutton(f,1) for f in listofrag] 
+    return " ".join(buts)
 @app.route('/rag')
 def dorag():
-    return "i would run a rag command here"
+    name=request.args.get('name')
+    rep=request.args.get('repeat',default=1,type=int)
+    return "i would run a rag command here:rag {0} {1}".format(name,rep)
+
+def ragbutton(s,rep):
+    return '''<button onclick="window.location.href='/dorag?name={0}&repeat={1}'">{0}</button>'''.format(s,rep) #later change to a form or something so we can also read the repeats setting. or simply reserve the page
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
