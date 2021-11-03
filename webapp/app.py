2c01ac6ae639a1953e576129cabb672dc5adc849
tilnow <44685516+tilnow@users.noreply.github.com>
2021-11-03 15:32:44 +0200

    add real rag

--- webapp/app.py
+++ webapp/app.py
@@ -1,10 +1,13 @@
+#very primitive setup just to see it works. next step is to extract the rag, etc. commands from iamz1 and turn them into a stand-alone library. or better, a service
+
 import os
-from flask import Flask, request
+from flask import Flask, request, redirect
 
 app = Flask(__name__)
 listofrag=os.listdir("/home/pi/SpiderPi/ActionGroups")
 listofrag=[f[:-4] for f in listofrag if f[-4:] in [".csv",".d6a"]]
 listofrag=list(set(listofrag))
+WHEREIRUNUNDER="/home/pi/test/iamz1/"
 
 @app.route('/')
 def index():
@@ -15,6 +18,11 @@ def index():
 def dorag():
     name=request.args.get('name')
     rep=request.args.get('repeat',default=1,type=int)
+    subprocess.Popen(['/usr/bin/python3', 'rag.py', name]+list(rep),
+           cwd=WHEREIRUNDIR,
+           stdout=subprocess.PIPE, 
+           stderr=subprocess.STDOUT)
+    return redirect("/")
     return "i would run a rag command here:rag {0} {1}".format(name,rep)
 
 def ragbutton(s,rep):
