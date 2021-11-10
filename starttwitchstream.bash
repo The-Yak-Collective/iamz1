36caecfe29eb13ef8704e19a5712d875dc2a8b30
tilnow <44685516+tilnow@users.noreply.github.com>
2021-11-10 15:48:03 +0200

    Update starttwitchstream.bash

--- starttwitchstream.bash
+++ starttwitchstream.bash
@@ -10,12 +10,12 @@ QUALITY="ultrafast"  # one of the many FFMPEG preset
 AUDIO_RATE="44100"
 
 #STREAM_KEY="$1" # use the terminal command Streaming streamkeyhere to stream your video to twitch or justin
-SERVER="live-sjc" # twitch server in California, see http://bashtech.net/twitch/ingest.php to change
+SERVER="fra02.contribute.live-video.net" # twitch server in frankfurt, see http://stream.twitch.tv/ingests to change
 
 ffmpeg -re -f v4l2 -i /dev/video0 \
 -c:v libx264 -preset veryfast -maxrate 1000k \
--bufsize 6000k -pix_fmt yuv420p -g 50 -an \
--f flv "rtmp://$SERVER.twitch.tv/app/$STREAM_KEY"
+-bufsize 1000k -r 10 -tune zerolatency -pix_fmt yuv420p -g 50 -an \
+-f flv "rtmp://$SERVER/app/$STREAM_KEY"
 #ffmpeg -f x11grab -s "$INRES" -r "$FPS" -i :0.0 -f alsa -i pulse -f flv -ac 2 -ar $AUDIO_RATE \
 #-vcodec libx264 -g $GOP -keyint_min $GOPMIN -b:v $CBR -minrate $CBR -maxrate $CBR -pix_fmt yuv420p\
 #-s $OUTRES -preset $QUALITY -tune film -acodec libmp3lame -threads $THREADS -strict normal \
