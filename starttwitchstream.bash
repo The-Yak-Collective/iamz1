993d9ea9d47a414458c1efc0d97a3f8bc6b8eb60
tilnow <44685516+tilnow@users.noreply.github.com>
2021-11-10 15:15:41 +0200

    Update starttwitchstream.bash

--- starttwitchstream.bash
+++ starttwitchstream.bash
@@ -1,6 +1,6 @@
 
-INRES="640x480" # input resolution
-OUTRES="640x480" # output resolution
+#INRES="1920x1080" # input resolution
+#OUTRES="1920x1080" # output resolution
 FPS="15" # target FPS
 GOP="30" # i-frame interval, should be double of FPS,
 GOPMIN="15" # min i-frame interval, should be equal to fps,
@@ -11,10 +11,14 @@ AUDIO_RATE="44100"
 
 #STREAM_KEY="$1" # use the terminal command Streaming streamkeyhere to stream your video to twitch or justin
 SERVER="live-sjc" # twitch server in California, see http://bashtech.net/twitch/ingest.php to change
- 
-ffmpeg -f flv -video_size "$INRES" -r "$FPS" -i /dev/video0 -f alsa -i pulse -f flv -ac 2 -ar $AUDIO_RATE \
--vcodec libx264 -g $GOP -keyint_min $GOPMIN -b:v $CBR -minrate $CBR -maxrate $CBR \
--s $OUTRES -preset $QUALITY -tune film -acodec libmp3lame -threads $THREADS -strict normal \
--bufsize $CBR "rtmp://$SERVER.twitch.tv/app/$STREAM_KEY"
+
+ffmpeg -re -f v4l2 -i /dev/video0 \
+-c:v libx264 -preset veryfast -maxrate 1000k \
+-bufsize 6000k -pix_fmt yuv420p -g 50 -an \
+-f flv "rtmp://$SERVER.twitch.tv/app/$STREAM_KEY"
+#ffmpeg -f x11grab -s "$INRES" -r "$FPS" -i :0.0 -f alsa -i pulse -f flv -ac 2 -ar $AUDIO_RATE \
+#-vcodec libx264 -g $GOP -keyint_min $GOPMIN -b:v $CBR -minrate $CBR -maxrate $CBR -pix_fmt yuv420p\
+#-s $OUTRES -preset $QUALITY -tune film -acodec libmp3lame -threads $THREADS -strict normal \
+#-bufsize $CBR "rtmp://$SERVER.twitch.tv/app/$STREAM_KEY"
 
 
