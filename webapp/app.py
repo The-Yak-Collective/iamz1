85383a5c8a9fc0dd7f917471f1b1777b49b387e5
tilnow <44685516+tilnow@users.noreply.github.com>
2021-11-10 16:10:42 +0200

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -61,7 +61,7 @@ def unloadbut():
 def twitchintegrate():
     return ('''
 <iframe
-    src="https://player.twitch.tv/?yakrovers&parent=www.yakcollective.org"
+    src="https://player.twitch.tv/?yakrovers&parent=wwg.rovers.yakcollective.org"
     height="500"
     width="500"
     allowfullscreen="<allowfullscreen>">
