c72cfa94e8fff61e03df27f417ef3510e369cf50
tilnow <44685516+tilnow@users.noreply.github.com>
2021-11-10 15:05:15 +0200

    now stream from camera

--- starttwitchstream.bash
+++ starttwitchstream.bash
@@ -12,7 +12,7 @@ AUDIO_RATE="44100"
 #STREAM_KEY="$1" # use the terminal command Streaming streamkeyhere to stream your video to twitch or justin
 SERVER="live-sjc" # twitch server in California, see http://bashtech.net/twitch/ingest.php to change
  
-ffmpeg -f x11grab -s "$INRES" -r "$FPS" -i :0.0 -f alsa -i pulse -f flv -ac 2 -ar $AUDIO_RATE \
+ffmpeg -f x11grab -s "$INRES" -r "$FPS" -i /dev/video0 -f alsa -i pulse -f flv -ac 2 -ar $AUDIO_RATE \
 -vcodec libx264 -g $GOP -keyint_min $GOPMIN -b:v $CBR -minrate $CBR -maxrate $CBR \
 -s $OUTRES -preset $QUALITY -tune film -acodec libmp3lame -threads $THREADS -strict normal \
 -bufsize $CBR "rtmp://$SERVER.twitch.tv/app/$STREAM_KEY"
