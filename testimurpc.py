28562df17119bdaae40daae2d54a0914ba494028
tilnow <44685516+tilnow@users.noreply.github.com>
2022-07-08 15:31:48 +0300

    show only integrated data in nicer format

--- testimurpc.py
+++ testimurpc.py
@@ -1,11 +1,15 @@
 import time
 import rpcservices
+def vec32short(x):
+    return ['%.2f' % y for y in x]
 
 while True:
     t=time.time()
-    print("imu:",rpcservices.getimudata())
-    print("pos:",rpcservices.getimupos())
-    print("leg:",rpcservices.leg_pos())
+#    print("imu:",rpcservices.getimudata())
+#    print("pos:",rpcservices.getimupos())
+    poses=rpcservices.getimupos()
+    print("pos:",vec32short(poses[0]),vec32short(poses[1]),vec32short(poses[2])
+#    print("leg:",rpcservices.leg_pos())
     t1=time.time()
     tts=0.1-(t1-t)
     print(t, t1, tts)
