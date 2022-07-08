493bd2eb3dd9bfc009d5fb5f4badc3bed9e34553
tilnow <44685516+tilnow@users.noreply.github.com>
2022-07-08 14:57:45 +0300

    add calibration first draft

--- imurpc.py
+++ imurpc.py
@@ -20,20 +20,25 @@ mpu=None
 
 RES6DOF = 0.1 #accuracy of raw data = every 100 ms. play with it...
 PORTFORIMU=9504 #IMU data port
-
+calib=[0,0,0,0,0,0,0]
+pos=[0,0,0]
+angle=[0,0,0]
 lastrecorded=(0,[]) #change a bit as we need to have continuous
 rawdata=[] #need some way to limit this to say last 1000 datums, etc
 TOOBIG=10000
 ENOUGH=1000
 def tick():
-    global rawdata
+    global rawdata, calib, pos, angle #not really raw any more data as we calibrate it...
     while True:
         st=time.time()
         t=int(st*1000) #time in ms
         eventdata=read6dof()
+        for i in len(eventdata):
+            eventdata[i]=eventdata[i]-calib[i]
         rawdata.append((st,eventdata)) #t is simply too large for xml int
         if len(rawdata)>TOOBIG:
             del rawdata[TOOBIG-ENOUGH:]
+        #and now (not yet here) calculate pos and angle
         t2=time.time()
         timelefttosleep=st+0.1-t2
         if timelefttosleep>0:
@@ -84,9 +89,21 @@ def read6dof():
     
 def init6dof():
     global mpu
+    global calib
+    t1=time.time()
     mpu = MPU.mpu6050(0x68)
     mpu.set_gyro_range(mpu.GYRO_RANGE_2000DEG)
     mpu.set_accel_range(mpu.ACCEL_RANGE_2G)
+    for i in range(100):
+        readit=read6dof()
+        data.append(readit) #100 readings. now lets average them
+        for i in len(readit):
+            calib[i]=calib[i]+readit[i]
+    for i in len(calib):
+        calib[i]=claib[i]/100
+    t2=time.time()
+    print(calib, t2-t1)) #for now, we are also zeroing the z axis. oh well...
+    
 
 
 
