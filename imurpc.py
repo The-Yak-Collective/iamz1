093d3065c8c5083010152142a5765b93600ca58a
tilnow <44685516+tilnow@users.noreply.github.com>
2022-07-08 13:49:25 +0300

    first draft imu data server

---
+++ imurpc.py
@@ -0,0 +1,91 @@
+#service that returns current gyro, acc, temp and also does integration and keeps a position log, so you can compare. or maybe only recalls compared to 0 time.
+#for now, use very low quality pos estimation
+import json
+from xmlrpc.server import SimpleXMLRPCServer
+from xmlrpc.server import SimpleXMLRPCRequestHandler
+import time
+import servo_util
+import csv
+import threading
+
+import os
+
+import Mpu6050class as MPU
+import sys
+import rpcservices
+
+from dotenv import load_dotenv, find_dotenv
+
+mpu=None
+
+RES6DOF = 0.1 #accuracy of raw data = every 100 ms. play with it...
+PORTFORIMU=9504 #IMU data port
+
+lastrecorded=(0,[]) #change a bit as we need to have continuous
+rawdata=[] #need some way to limit this to say last 1000 datums, etc
+TOOBIG=10000
+ENOUGH=1000
+def tick():
+    global rawdata
+    st=time.time()
+    t=int(st*1000) #time in ms
+    eventdata=read6dof()
+    rawdata.append((t,eventdata))
+    if len(rawdata)>TOOBIG:
+        del rawdata[TOOBIG-ENOUGH:]
+    t2=time.time()
+    timelefttosleep=st+0.1-t2
+    if timelefttosleep>0:
+        time.sleep(timelefttosleep)#so we sample about each 0.1 seconds
+        #print(timelefttosleep)
+    else:
+        print('(read is slow) timelefttosleep=',timelefttosleep, st,t2)
+
+def main():
+    global timestamp
+
+    init6dof()
+
+    # Restrict to a particular path.
+    class RequestHandler(SimpleXMLRPCRequestHandler):
+        rpc_paths = ('/RPC2',)
+
+    # Create server
+    with SimpleXMLRPCServer(('localhost', PORTFORIMU),
+                            requestHandler=RequestHandler) as server:
+        server.register_introspection_functions()
+
+        # Register a function under a different name
+        def getimudata(): #no locking, for now, but probably should have...
+            global lastrecorded,rawdata
+#            t=time.time()
+#            if t-lastrecorded[0]>RES6DOF:
+#                lastrecorded=(t,read6dof())
+#            return (lastrecorded,rawdata[-1]) #needs work - as should only return the data from raw data
+            try:
+                return (rawdata[-1]) #needs work - as should only return the data from raw data
+            except:
+                return(0,[0,0,0,0,0,0,0]
+
+        server.register_function(getimudata, 'getimudata')
+        x=threading.Thread(target=tick, daemon=True) #when tick works, will open this and then getimudata should be pure "read form the stack"
+        x.start()
+        # Run the server's main loop
+        server.serve_forever()
+
+    
+def read6dof():
+    d=mpu.get_all_data()
+    res=[d[0]["x"],d[0]["y"],d[0]["z"],d[1]["x"],d[1]["y"],d[1]["z"],d[2]]
+    return res
+
+    
+def init6dof():
+    global mpu
+    mpu = MPU.mpu6050(0x68)
+    mpu.set_gyro_range(mpu.GYRO_RANGE_2000DEG)
+    mpu.set_accel_range(mpu.ACCEL_RANGE_2G)
+
+
+
+main() #so i can play with function order
--- rpcservices.py
+++ rpcservices.py
@@ -4,6 +4,7 @@ import json
 PORTFORLOGGING=9501 # later we will make this an env variable. for now, use 9500-9550
 PORTFORLEGS=9502 #position of each servo
 PORTFORRAG=9503 #send rag commands here
+PORTFORIMU=9504 #get 6dof (or better?) from here
 
 logging = xmlrpc.client.ServerProxy('http://localhost:'+str(PORTFORLOGGING))
 
@@ -25,3 +26,8 @@ ragserver = xmlrpc.client.ServerProxy('http://localhost:'+str(PORTFORRAG))
 
 def rag(name, reps=1,speedratio=1.0,modu=False,toldtowait=True):#dropping "savedata", as we have logger now
     return ragserver.rag(name,reps,speedratio,modu,toldtowait)
+
+imuserver = xmlrpc.client.ServerProxy('http://localhost:'+str(PORTFORIMU))
+
+def getimudata():#return pair of time and 7 element vector. later do better
+    return imuserver.getdata()
